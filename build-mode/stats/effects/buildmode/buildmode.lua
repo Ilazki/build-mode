9902e74b83cc3cefbf8e59ea1c5642eeab32ec4b
Ilazki <ws.ilazki@gmail.com>
2016-12-02 15:07:04 -0500

    Comment for later.  TODO:  consider adjusting build mode effects after more testing.

--- build-mode/stats/effects/buildmode/buildmode.lua
+++ build-mode/stats/effects/buildmode/buildmode.lua
@@ -1,6 +1,8 @@
 function init()
 end
 
+--- TODO:  Determine if these settings are good or if further adjustment is
+--- needed to keep build mode feeling fair despite its benefits.
 function update(dt)
    mcontroller.controlModifiers({
 		 groundMovementModifier = 0.4,
